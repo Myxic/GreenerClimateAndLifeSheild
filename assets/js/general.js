1e80a1e6b0ee12c973db7faaa3fe82c6efd3ac2e
Ezemenahi Benedict <ezemenahi56@gmail.com>
2023-09-21 13:59:12 +0100

    Initial commit

---
+++ assets/js/general.js
@@ -0,0 +1,16 @@
+
+addAdminModal.addEventListener("click", ()=> {
+    adminModal.showModal();
+});
+closeAdminModal.addEventListener("click", ()=> {
+    adminModal.close();
+});
+
+
+
+makeAnnouncement.addEventListener("click", ()=> {
+    announcementModal.showModal();
+});
+closeAnnouncementModal.addEventListener("click", ()=> {
+    announcementModal.close();
+});
---
+++ assets/js/variables.js
@@ -0,0 +1,86 @@
+const addEventModal = document.querySelector('#addEventModal');
+
+const viewEventButton = document.querySelector('#viewEventButton');
+
+const addADModal = document.querySelector('#addADModal');
+
+const addAdminModal = document.querySelector('#addAdminModal');
+
+const closeEventModal = document.querySelector('#closeEventModal');
+
+const closeAdminModal = document.querySelector('#closeAdminModal');
+
+const closeADmodel = document.querySelector('#closeADmodel');
+
+const removeADModal = document.querySelector('#removeADModal');
+
+const  removeAdminModal = document.querySelector('#removeAdminModal');
+
+const removeEventModal = document.querySelector('#removeEventModal');
+
+const settingsModal = document.querySelector('#settingsModal');
+
+const eventsModal = document.querySelector('#eventsModal');
+
+const adminModal = document.querySelector('#adminModal');
+
+const adModel = document.querySelector('#adModel');
+
+const ElectricalreportModal = document.querySelector('#ElectricalreportModal');
+
+const PlumbingReportModal = document.querySelector('#PlumbingReportModal');
+
+const CarpentryReportModal = document.querySelector('#CarpentryReportModal');
+
+const WindowReportModal = document.querySelector('#WindowReportModal');
+
+const OthersReportModal = document.querySelector('#OthersReportModal');
+
+const openSettingsModal = document.querySelector('#openSettingsModal');
+
+const closeSettingsModal = document.querySelector('#closeSettingsModal');
+
+const viewEventButtons = document.querySelectorAll('.viewEventsModal');
+
+// const viewEventsModal = document.querySelector('#viewEventsModal');
+
+const closeEventsModal = document.querySelector('#closeEventsModal');
+
+const viewElectricalreportModal = document.querySelector('#viewElectricalreportModal');
+
+const viewPlumbingReportModal = document.querySelector('#viewPlumbingReportModal');
+
+const viewCarpentryReportModal = document.querySelector('#viewCarpentryReportModal');
+
+const viewWindowReportModal = document.querySelector('#viewWindowReportModal');
+
+const viewOthersReportModal = document.querySelector('#viewOthersReportModal');
+
+const closeElectricalreportModal = document.querySelector('#closeElectricalreportModal');
+
+const closePlumbingReportModal = document.querySelector('#closePlumbingReportModal');
+
+const closeCarpentryReportModal = document.querySelector('#closeCarpentryReportModal');
+
+const closeWindowReportModal = document.querySelector('#closeWindowReportModal');
+
+const closeOthersReportModal = document.querySelector('#closeOthersReportModal');
+
+const adminContainer = document.querySelector('.Admins');
+
+const eventContainer = document.querySelector('EventsContext');
+
+const addAdminButton = document.querySelector('#addAdminButton');
+
+const removeAdminButton = document.querySelector('#removeAdminButton');
+
+const addEventButton = document.querySelector('#addEventButton');
+
+const removeEventButton = document.querySelector('#removeEventButton');
+
+const makeAnnouncement = document.querySelector('#makeAnnouncement');
+
+const announcementModal = document.querySelector('#announcementModal');
+
+const closeAnnouncementModal = document.querySelector('#closeAnnouncementModal');
+
